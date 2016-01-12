9f97c8c68a0c76488b14517a57f54dff6acf3f8e
Lee JiHun <jihyun9114@naver.com>
2016-01-12 23:18:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sony/calculatortest/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.sony.calculatortest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+    }
+    public void onItemSelected(View view){
+        btn = (Button)findViewById(view.getId());
+        textView.setText(btn.getText());
+    }
+}
